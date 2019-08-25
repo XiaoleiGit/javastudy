49477c5e74c6349e2c14b63c9998e6bad7d9191d
Xiaolei <winny_wxl@hotmail.com>
2019-08-25 16:03:10 +0200

    first commit

---
+++ src/main/java/com/javastudy/springboot/demoApp/App.java
@@ -0,0 +1,22 @@
+package com.javastudy.springboot.demoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping(value = "/")
+    public String helloWorld(){
+    	return "Hello World";
+    }
+    
+}
