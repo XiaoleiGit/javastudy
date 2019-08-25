f52bacddd810d65e43d75a757288991d74f8e56a
Xiaolei <winny_wxl@hotmail.com>
2019-08-25 17:07:43 +0200

    change to war

--- src/main/java/com/javastudy/springboot/demoApp/App.java
+++ src/main/java/com/javastudy/springboot/demoApp/App.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @RestController
-public class App 
+public class App extends SpringBootServletInitializer
 {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
